6d75ef1b94e85c4bec20d362afc25a6d67ccf34f
Anirudh Uppunda <abhat38@gmail.com>
2022-01-05 13:32:59 +0530

    make news fetch from repository for success api response pass

--- app/src/main/java/com/abhat/cleannews_compose/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/abhat/cleannews_compose/data/repository/NewsRepositoryImpl.kt
@@ -3,9 +3,21 @@ package com.abhat.cleannews_compose.data.repository
 import com.abhat.cleannews_compose.data.api.NewsApi
 import com.abhat.cleannews_compose.data.repository.state.NewsRepoState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class NewsRepositoryImpl(private val newsApi: NewsApi): NewsRepository {
     override suspend fun getNewsRss(url: String): Flow<NewsRepoState> {
-        TODO("Not yet implemented")
+        return flow {
+            try {
+                val response = newsApi.getNewsRssAsync(url).await()
+                response.channel?.items?.let {
+                    emit(NewsRepoState.Success(news = it))
+                } ?: run {
+                    emit(NewsRepoState.Error(Throwable("News items are null or empty")))
+                }
+            } catch (e: Exception) {
+                emit(NewsRepoState.Error(Throwable(e.localizedMessage)))
+            }
+        }
     }
 }
--- app/src/test/java/com/abhat/cleannews_compose/repository/NewsRepositoryTest.kt
+++ app/src/test/java/com/abhat/cleannews_compose/repository/NewsRepositoryTest.kt
@@ -2,11 +2,15 @@ package com.abhat.cleannews_compose.repository
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.abhat.cleannews_compose.data.api.NewsApi
+import com.abhat.cleannews_compose.data.models.Channel
+import com.abhat.cleannews_compose.data.models.Item
+import com.abhat.cleannews_compose.data.models.Rss
 import com.abhat.cleannews_compose.data.repository.NewsRepositoryImpl
 import com.abhat.cleannews_compose.data.repository.state.NewsRepoState
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
@@ -29,12 +33,20 @@ class NewsRepositoryTest {
     fun `given success response from api, when news repo called, then success repo state is returned` () {
         runBlocking {
             val expectedState = NewsRepoState.Success(listOf())
-            whenever(newsApi.getNewsRssAsync("")).thenReturn(CompletableDeferred())
+            whenever(newsApi.getNewsRssAsync("")).thenReturn(
+                CompletableDeferred(
+                    Rss(
+                        channel = Channel(
+                            items = listOf()
+                        )
+                    )
+                )
+            )
             val newsRepository = NewsRepositoryImpl(newsApi)
 
-            val actualState = newsRepository.getNewsRss("")
+            val actualState = newsRepository.getNewsRss("").toList()
 
-            Assert.assertEquals(expectedState, actualState)
+            Assert.assertEquals(expectedState, actualState[0])
         }
     }
 }

9bb20fad11ee1c927ca3b4569a68cc91ea3d7664
Anirudh Uppunda <abhat38@gmail.com>
2022-01-05 13:23:52 +0530

    add test for fetching news repo success logic

--- app/src/main/java/com/abhat/cleannews_compose/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/abhat/cleannews_compose/data/repository/NewsRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.abhat.cleannews_compose.data.repository
 
+import com.abhat.cleannews_compose.data.api.NewsApi
 import com.abhat.cleannews_compose.data.repository.state.NewsRepoState
 import kotlinx.coroutines.flow.Flow
 
-class NewsRepositoryImpl: NewsRepository {
+class NewsRepositoryImpl(private val newsApi: NewsApi): NewsRepository {
     override suspend fun getNewsRss(url: String): Flow<NewsRepoState> {
         TODO("Not yet implemented")
     }
--- app/src/test/java/com/abhat/cleannews_compose/repository/NewsRepositoryTest.kt
+++ app/src/test/java/com/abhat/cleannews_compose/repository/NewsRepositoryTest.kt
@@ -1,4 +1,40 @@
 package com.abhat.cleannews_compose.repository
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.abhat.cleannews_compose.data.api.NewsApi
+import com.abhat.cleannews_compose.data.repository.NewsRepositoryImpl
+import com.abhat.cleannews_compose.data.repository.state.NewsRepoState
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
 class NewsRepositoryTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var newsApi: NewsApi
+
+    @Before
+    fun setup() {
+        newsApi = mock()
+    }
+
+    @Test
+    fun `given success response from api, when news repo called, then success repo state is returned` () {
+        runBlocking {
+            val expectedState = NewsRepoState.Success(listOf())
+            whenever(newsApi.getNewsRssAsync("")).thenReturn(CompletableDeferred())
+            val newsRepository = NewsRepositoryImpl(newsApi)
+
+            val actualState = newsRepository.getNewsRss("")
+
+            Assert.assertEquals(expectedState, actualState)
+        }
+    }
 }

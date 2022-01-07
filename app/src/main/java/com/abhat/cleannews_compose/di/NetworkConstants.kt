1ca657e77f1e90a3129504fda82c943c97f89c19
Anirudh Uppunda <abhat38@gmail.com>
2022-01-07 13:57:50 +0530

    add di files

---
+++ app/src/main/java/com/abhat/cleannews_compose/di/NetworkConstants.kt
@@ -0,0 +1,8 @@
+package com.abhat.cleannews_compose.di
+
+/**
+ * Created by Anirudh Uppunda on 19,October,2020
+ */
+object NetworkConstants {
+    const val BASE_URL = "https://ddnews.gov.in/" // Add your base url for retrofit here
+}
